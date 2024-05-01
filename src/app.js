9b93c0b70ebea6027d9bb0b92961a0df944ce9e5
angelcuevas <angel.p.cuevas@gmail.com>
2024-05-01 00:10:11 -0300

    added: module use example

---
+++ src/app.js
@@ -0,0 +1,6 @@
+const { sumar, restar, multiplicar, dividir } = require("math-58195");
+
+console.log("SUMA", sumar(1,2))
+console.log("RESTA", restar(1,2))
+console.log("MULTIPLICAR", multiplicar(1,2))
+console.log("DIVIDIR", dividir(1,2))
